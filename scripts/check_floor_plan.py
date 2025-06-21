aabd7e13d25e454f88d6c666fec1760e1b0b4dc0
Zahurul-Master <34090649+arad71@users.noreply.github.com>
2025-06-21 16:09:40 +0800

    Create check_floor_plan.py

---
+++ scripts/check_floor_plan.py
@@ -0,0 +1,26 @@
+{
+  "rooms": [
+    {
+      "type": "Bedroom",
+      "dimensions": {"length": 3.0, "width": 2.8, "height": 2.4},
+      "window_area": 1.5,
+      "tags": ["master"]
+    },
+    {
+      "type": "Kitchen",
+      "dimensions": {"length": 3.5, "width": 2.5, "height": 2.4},
+      "window_area": 1.0
+    }
+  ],
+  "doors": [{"width": 0.8}],
+  "stairs": [{"width": 0.9}],
+  "construction": {
+    "insulation": {
+      "ceiling": 3.0,
+      "external_wall": 2.5,
+      "floor": 1.5
+    },
+    "external_wall_area": 120,
+    "glass_area": 45
+  }
+}
